f22d5052454d84fc3b9daa71b7f40ce5f6bedfa9
FarheenNaz-07 <farheen.naz711@gmail.com>
2025-03-08 20:21:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) //We are using nodejs not browser
+console.log(3
+    +3) // Code readibility should be high
+
+console.log("Farheen Naz")
+
+let name = "Farheen"
+let age = 21
+let isLoggedin = true
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolen => true/false
+// null => standalone value (null matlab empty and iska "" matlab null nahi hain.
+// ye "" khali string hain iska type defined hogaya hain string)
+// undefined => Abhi value assigned nahi hui hain.
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) //undefined(undefined apne ap mea ek type hain js ke andar)
+console.log(typeof null) //object
